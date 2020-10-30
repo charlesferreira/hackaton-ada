2f100533eae65dc4ee87fa6c3b5d69f9e916004c
Charles Ferreira <charles.dev@live.com>
2020-10-29 22:59:04 -0300

    Corrige rotas

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,24 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AppComponent } from './app.component';
 import { PainelAdminComponent } from './components/admin/painel-admin/painel-admin.component';
 import { PainelAtividadeAtualComponent } from './components/public/painel-atividade-atual/painel-atividade-atual.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: AppComponent,
-    children: [
-      {
-        path: '',
-        component: PainelAtividadeAtualComponent,
-      },
-      {
-        path: 'admin',
-        component: PainelAdminComponent,
-      },
-    ],
+    component: PainelAtividadeAtualComponent,
+  },
+  {
+    path: 'admin',
+    component: PainelAdminComponent,
   },
 ];
 
